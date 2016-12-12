8aa87c89f3c53c4a36c0cc0ffcf53550705f0f7e
Ivan Rufino Martins <ivan.rufino.m@gmail.com>
2016-12-12 15:12:33 -0200

    Criando o template principal e htaccess - 3a

--- application/config/config.php
+++ application/config/config.php
@@ -368,9 +368,9 @@ $config['encryption_key'] = sha1(md5('ivanrufino'));
 |
 */
 $config['sess_driver'] = 'database';
-$config['sess_cookie_name'] = 'ci_sessions';
+$config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
